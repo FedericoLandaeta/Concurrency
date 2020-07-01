79accbccc28c0b8351f02269a61cb881e6c26528
FedericoLandaeta <65091166+FedericoLandaeta@users.noreply.github.com>
2020-07-01 19:30:00 -0400

    Added parallelSum, still not working correctly.

--- Summation.java
+++ Summation.java
@@ -1,9 +1,18 @@
 package concurrency;
 
 class Summation extends Thread {
-
-	//private int[] arr;
+	int s;
 	
+	public Summation(int[] arr, int i, int min) {
+	}
+	
+	public int getSum()
+
+	{
+
+	return s;
+
+	}
 	public static int sum(int[] arr)
 	{
 		int sum = 0;
@@ -13,10 +22,27 @@ class Summation extends Thread {
 		return sum;
 	}
 	
-	/**public static int parallelSum(int[] arr)
+	public static int parallelSum(int[] arr, int threads)
 	{
+		int length = (int) Math.ceil(1.0 * arr.length / threads);
+		Summation[] summation = new Summation[threads];
+		Thread[] thread = new Thread[threads];
+		for (int i=0; i <threads; i++) {
+			summation[i] = new Summation(arr, i*length, Math.min((i+1)*length, arr.length));
+			thread[i] = new Thread(summation[i]);
+			thread[i].start();
+		}
+		try {
+			for (Thread t : thread) {
+				t.join();
+			}
+		}catch (InterruptedException ie) {
+		}
+		int total = 0;
+		for (Summation sum : summation) {
+			total += sum.getSum();
+		}
+		return total;
 		
-		return parrallelSum;
-		
-	}**/
+	}
 }
