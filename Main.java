f5212aee83e72cccd3573d512d19f339004a5437
FedericoLandaeta <65091166+FedericoLandaeta@users.noreply.github.com>
2020-07-01 22:44:28 -0400

    added comments

--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ package concurrency;
 import java.util.Random;
 
 public class Main {
+	//initializing an array of 200 million
 	static int[] arr = new int[200000000];
 	public static void main(String[] args)
 	{
+		//adding random numbers 1-10 into the array
 		Random rand = new Random();
-		
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = rand.nextInt(10);
 		}
@@ -15,6 +16,7 @@ public class Main {
 		System.out.println("Sum: " + Summation.sum(arr));
 		System.out.println("Time for single thread summation: " + (System.currentTimeMillis() - start) + " ms" );
 		start = System.currentTimeMillis();
+		//parallelSum is still not working
 		System.out.println("Sum: " + Summation.parallelSum(arr, Runtime.getRuntime().availableProcessors()));
 		System.out.println("Time for parallel summation: " + (System.currentTimeMillis() - start) +" ms");
 
